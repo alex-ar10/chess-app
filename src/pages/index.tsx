8e2bf7f611ff32bc6b11ab85b3a4a359dd5c12cc
alex-ar10 <roehr.alexander@gmail.com>
2023-08-09 15:24:43 +0200

    add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/Header/Header";
 import ChessGame from "@/components/ChessGame/ChessGame";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Queen&apos;s Quest</title>
+      </Head>
       <div className="flex flex-col justify-center items-center">
         <Header pageTitle="Queen's Quest" />
         <ChessGame />
